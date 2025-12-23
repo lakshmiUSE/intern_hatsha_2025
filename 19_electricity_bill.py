978b7bd416c08fbd0303937892a2c0b5921837a5
hv7661863-cmd <hv7661863@gmail.com>
2025-12-23 09:55:39 +0530

    Update 19_electricity_bill.py

--- 19_electricity_bill.py
+++ 19_electricity_bill.py
@@ -1,1 +1,7 @@
 # WAP to calculate electricity bill.
+units = float(input("Enter units consumed: "))
+rate = float(input("Enter rate per unit: "))
+
+bill = units * rate
+
+print("Electricity Bill = ₹", bill)
