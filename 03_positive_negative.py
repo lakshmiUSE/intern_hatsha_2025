5db4a53c221d8c7ca80f52380b831666dfdc4726
hv7661863-cmd <hv7661863@gmail.com>
2025-12-23 09:31:35 +0530

    Update 03_positive_negative.py

--- 03_positive_negative.py
+++ 03_positive_negative.py
@@ -1,1 +1,9 @@
 # WAP to check whether a given number is positive or negative.
+
+num = float(input("Enter a number: "))
+if num > 0:
+   print(f"{num} is a positive number")
+elif num < 0:
+   print(f"{num} is a negative number")
+else:
+   print("The number is zero")
