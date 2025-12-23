11e256dbfb609d48bccaf8fd32c4fc739718d3db
hv7661863-cmd <hv7661863@gmail.com>
2025-12-23 09:45:20 +0530

    Update 12_vowel_consonant.py

--- 12_vowel_consonant.py
+++ 12_vowel_consonant.py
@@ -1,1 +1,7 @@
 # WAP to check whether an alphabet is vowel or consonant.
+ch = input("Enter an alphabet: ")
+
+if ch.lower() in ['a', 'e', 'i', 'o', 'u']:
+    print("It is a Vowel")
+else:
+    print("It is a Consonant")
