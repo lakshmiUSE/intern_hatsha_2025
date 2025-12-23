d792bc745776fbc6a45ea35a652ad5116f06a6d6
hv7661863-cmd <hv7661863@gmail.com>
2025-12-23 09:51:41 +0530

    Update 16_coordinate_quadrant.py

--- 16_coordinate_quadrant.py
+++ 16_coordinate_quadrant.py
@@ -1,1 +1,18 @@
 # WAP to determine quadrant of a coordinate.
+x = float(input("Enter x coordinate: "))
+y = float(input("Enter y coordinate: "))
+
+if x > 0 and y > 0:
+    print("Point lies in First Quadrant")
+elif x < 0 and y > 0:
+    print("Point lies in Second Quadrant")
+elif x < 0 and y < 0:
+    print("Point lies in Third Quadrant")
+elif x > 0 and y < 0:
+    print("Point lies in Fourth Quadrant")
+elif x == 0 and y == 0:
+    print("Point lies at the Origin")
+elif x == 0:
+    print("Point lies on Y-axis")
+elif y == 0:
+    print("Point lies on X-axis")
