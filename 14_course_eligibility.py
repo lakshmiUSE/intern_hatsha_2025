cba7d05408ca02e05d55a486ea2d222979974b66
hv7661863-cmd <hv7661863@gmail.com>
2025-12-23 09:48:19 +0530

    Update 14_course_eligibility.py

--- 14_course_eligibility.py
+++ 14_course_eligibility.py
@@ -1,1 +1,11 @@
 # WAP to check professional course eligibility.
+marks = float(input("Enter your percentage: "))
+
+if marks >= 85:
+    print("Eligible for Engineering and Medical courses")
+elif marks >= 70:
+    print("Eligible for Engineering courses")
+elif marks >= 60:
+    print("Eligible for Degree professional courses")
+else:
+    print("Not eligible for professional courses")
