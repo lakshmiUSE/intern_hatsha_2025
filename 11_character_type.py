4dfa44bc4d67f00c80206f74d5fbdd678ce98591
hv7661863-cmd <hv7661863@gmail.com>
2025-12-23 09:44:07 +0530

    Update 11_character_type.py

--- 11_character_type.py
+++ 11_character_type.py
@@ -1,1 +1,9 @@
 # WAP to check whether character is alphabet, digit, or special character.
+ch = input("Enter a character: ")
+
+if ch.isalpha():
+    print("It is an Alphabet")
+elif ch.isdigit():
+    print("It is a Digit")
+else:
+    print("It is a Special Character")
