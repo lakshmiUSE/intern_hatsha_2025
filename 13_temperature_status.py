971f22a9448a788a695bd573527df060153ec177
hv7661863-cmd <hv7661863@gmail.com>
2025-12-23 09:47:06 +0530

    Update 13_temperature_status.py

--- 13_temperature_status.py
+++ 13_temperature_status.py
@@ -1,1 +1,13 @@
 # WAP to display temperature status.
+temp = float(input("Enter temperature in °C: "))
+
+if temp < 0:
+    print("Freezing weather")
+elif temp >= 0 and temp < 20:
+    print("Cold weather")
+elif temp >= 20 and temp < 30:
+    print("Normal temperature")
+elif temp >= 30 and temp < 40:
+    print("Hot temperature")
+else:
+    print("Very hot temperature")
