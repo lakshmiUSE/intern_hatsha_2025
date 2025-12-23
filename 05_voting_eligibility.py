e5c91d796026546bd516aff69d8f0e2fbde8d82b
hv7661863-cmd <hv7661863@gmail.com>
2025-12-23 09:35:39 +0530

    Update 05_voting_eligibility.py

--- 05_voting_eligibility.py
+++ 05_voting_eligibility.py
@@ -1,1 +1,6 @@
 # WAP to read the age of a candidate and determine whether eligible for voting.
+age = int(input("Enter the candidate's age: "))
+if age >= 18:
+    print("The candidate is eligible for voting.")
+else:
+    print("The candidate is not eligible for voting.")
