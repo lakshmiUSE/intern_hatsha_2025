2e86220769b08ca369f2d77b3840d0f33a8d28fb
hv7661863-cmd <hv7661863@gmail.com>
2025-12-23 09:39:01 +0530

    Update 07_height_category.py

--- 07_height_category.py
+++ 07_height_category.py
@@ -1,1 +1,11 @@
 # WAP to categorize person based on height.
+height = float(input("Enter height in centimeters: "))
+if height < 150.0:
+    category = "small"
+elif height >= 150.0 and height < 165.0:
+    category = "Average height"
+elif height >= 165.0 and height <= 195.0:
+    category = "Taller"
+else:
+    category = "Abnormal height"
+print(f"The person is categorized as: {category}")
