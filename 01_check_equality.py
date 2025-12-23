27c0ba50aead3675fe00270740972807293fa856
hv7661863-cmd <hv7661863@gmail.com>
2025-12-23 09:27:32 +0530

    Update 01_check_equality.py

--- 01_check_equality.py
+++ 01_check_equality.py
@@ -1,1 +1,9 @@
 # WAP to accept two integers and check whether they are equal or not.
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+
+if num1 == num2:
+  print("the numbers are equal.")
+else:
+  print("the numbers are not equal.")
+  
