4a62e45246aff74c923e54b610e6611c2d0fa238
hv7661863-cmd <hv7661863@gmail.com>
2025-12-23 09:57:18 +0530

    Update 20_grade_description.py

--- 20_grade_description.py
+++ 20_grade_description.py
@@ -1,1 +1,15 @@
 # WAP to display description based on grade.
+grade = input("Enter your grade (A, B, C, D, F): ").upper()
+
+if grade == 'A':
+    print("Excellent")
+elif grade == 'B':
+    print("Good")
+elif grade == 'C':
+    print("Average")
+elif grade == 'D':
+    print("Needs improvement")
+elif grade == 'F':
+    print("Fail")
+else:
+    print("Invalid grade entered. Please enter a grade between A and F.")
