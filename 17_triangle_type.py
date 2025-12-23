c8d25c416486fa27738711cb1864606b2a010466
hv7661863-cmd <hv7661863@gmail.com>
2025-12-23 09:52:35 +0530

    Update 17_triangle_type.py

--- 17_triangle_type.py
+++ 17_triangle_type.py
@@ -1,1 +1,15 @@
 # WAP to check triangle type.
+a = float(input("Enter first side: "))
+b = float(input("Enter second side: "))
+c = float(input("Enter third side: "))
+
+# Check valid triangle
+if a + b > c and a + c > b and b + c > a:
+    if a == b == c:
+        print("Equilateral Triangle")
+    elif a == b or b == c or a == c:
+        print("Isosceles Triangle")
+    else:
+        print("Scalene Triangle")
+else:
+    print("Not a valid triangle")
