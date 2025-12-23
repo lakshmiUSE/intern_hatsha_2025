ba394e4bb4d0c9d25ce9d07ea844c28180d00248
hv7661863-cmd <hv7661863@gmail.com>
2025-12-23 09:53:53 +0530

    Update 18_triangle_possible.py

--- 18_triangle_possible.py
+++ 18_triangle_possible.py
@@ -1,1 +1,9 @@
 # WAP to check if triangle can be formed using angles.
+a = float(input("Enter first angle: "))
+b = float(input("Enter second angle: "))
+c = float(input("Enter third angle: "))
+
+if a > 0 and b > 0 and c > 0 and (a + b + c == 180):
+    print("Triangle can be formed")
+else:
+    print("Triangle cannot be formed")
